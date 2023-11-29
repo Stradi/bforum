a7fb19e405c3f1b50064d386ae7836d9407a6ce4
Batin Evirgen <batinevirgen@gmail.com>
2023-11-29 17:36:52 +0300

    docs(backend): add JSDoc to thread schema

--- apps/backend/src/database/schemas/thread.ts
+++ apps/backend/src/database/schemas/thread.ts
@@ -2,6 +2,27 @@ import { relations } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { nodesTable } from "./node"; // eslint-disable-line import/no-cycle -- there's nothing we can do
 
+/**
+ * ### `Thread` Schema
+ *
+ * `Thread` refers to a discussion in a forum. In bForum, `Thread` doesn't have a body attached to it.
+ * Instead, it has multiple `Post`s. `Post`s are the actual messages in a `Thread`.
+ *
+ * An example of a `Thread` is "What are you currently reading?" or "What are you currently watching?".
+ * Remember, `Thread` doesn't have a body, so the first post in the thread will be the body.
+ *
+ * In the future, we should add a `type` column to `Thread`. This will allow us to have different types of
+ * `Thread`s. For example, we can have a `Thread` type called "Poll". In front-end, we can render the
+ * `Thread` differently depending on the `type`.
+ *
+ * Apart from default columns, `Thread` has the following columns:
+ *
+ * | Column Name | Type | Description |
+ * | ----------- | ---- | ----------- |
+ * | `node_id` | `integer` | The ID of the `Node` that this `Thread` belongs to. |
+ * | `name` | `text` | The name of the `Thread`. |
+ * | `slug` | `text` | The slug of the `Thread`. |
+ */
 export const threadsTable = sqliteTable("threads", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   created_at: integer("created_at", { mode: "timestamp" }),
