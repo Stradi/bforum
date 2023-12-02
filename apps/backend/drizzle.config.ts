3d2a5da28fea8ff151bc0bf9429205135955e970
Batin Evirgen <batinevirgen@gmail.com>
2023-12-02 16:44:41 +0300

    chore(backend): make Drizzle Studio work

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -3,4 +3,8 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/database/schemas/**/*.ts",
   out: "./drizzle",
+  driver: "better-sqlite",
+  dbCredentials: {
+    url: "./main.db",
+  },
 } satisfies Config;
