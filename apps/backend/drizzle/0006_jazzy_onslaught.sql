25de4c1323a4ebd23c0688469bc785bebb7cb42b
Batin Evirgen <batinevirgen@gmail.com>
2023-12-02 17:14:18 +0300

    fix(backend): use composite primary key in `account-group` table

--- apps/backend/drizzle/0006_jazzy_onslaught.sql
+++ apps/backend/drizzle/0006_jazzy_onslaught.sql
@@ -1,9 +1,7 @@
 CREATE TABLE `account_group` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`created_at` integer,
-	`updated_at` integer,
 	`account_id` integer NOT NULL,
 	`group_id` integer NOT NULL,
+	PRIMARY KEY(`account_id`, `group_id`),
 	FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`) ON UPDATE no action ON DELETE no action
 );
--- apps/backend/src/database/schemas/account-group.ts
+++ apps/backend/src/database/schemas/account-group.ts
@@ -1,21 +1,25 @@
 import { relations } from "drizzle-orm";
 import type { AnySQLiteColumn } from "drizzle-orm/sqlite-core";
-import { integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { integer, primaryKey, sqliteTable } from "drizzle-orm/sqlite-core";
 import { accountsTable } from "./account"; // eslint-disable-line import/no-cycle -- there's nothing we can do
 import { groupsTable } from "./group"; // eslint-disable-line import/no-cycle -- there's nothing we can do
 
-export const accountGroupTable = sqliteTable("account_group", {
-  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
-  created_at: integer("created_at", { mode: "timestamp" }),
-  updated_at: integer("updated_at", { mode: "timestamp" }),
-
-  account_id: integer("account_id", { mode: "number" })
-    .notNull()
-    .references((): AnySQLiteColumn => accountsTable.id),
-  group_id: integer("group_id", { mode: "number" })
-    .notNull()
-    .references((): AnySQLiteColumn => groupsTable.id),
-});
+export const accountGroupTable = sqliteTable(
+  "account_group",
+  {
+    account_id: integer("account_id", { mode: "number" })
+      .notNull()
+      .references((): AnySQLiteColumn => accountsTable.id),
+    group_id: integer("group_id", { mode: "number" })
+      .notNull()
+      .references((): AnySQLiteColumn => groupsTable.id),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.account_id, table.group_id] }),
+    };
+  }
+);
 
 export const accountGroupRelations = relations(
   accountGroupTable,
