4f92345e00575d9c35946931ef9507c87ba225b3
Batin Evirgen <batinevirgen@gmail.com>
2023-12-06 22:34:41 +0300

    fix(backend): make authMiddleware not throw error unless a flag is on

--- apps/backend/src/middlewares/auth-middleware.ts
+++ apps/backend/src/middlewares/auth-middleware.ts
@@ -5,16 +5,21 @@ import { BaseError } from "../utils/errors";
 
 const authService = new AuthService();
 
-export default function authMiddleware(): MiddlewareHandler {
+export default function authMiddleware(require = false): MiddlewareHandler {
   return async (ctx, next) => {
     const authCookie = getCookie(ctx, "auth-token");
     if (!authCookie) {
-      throw new BaseError({
-        statusCode: 401,
-        code: "NO_AUTH_TOKEN",
-        message: "Auth token does not exist",
-        action: "Provide an auth token in the 'auth-token' cookie",
-      });
+      if (require) {
+        throw new BaseError({
+          statusCode: 401,
+          code: "NO_AUTH_TOKEN",
+          message: "Auth token does not exist",
+          action: "Provide an auth token in the 'auth-token' cookie",
+        });
+      } else {
+        await next();
+        return;
+      }
     }
 
     const jwtPayload = await authService.extractJwtPayload(authCookie);
