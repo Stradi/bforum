360b40c7c3483b365c017570632fb5b3da1d23cf
Batin Evirgen <batinevirgen@gmail.com>
2023-12-09 01:15:59 +0300

    refactor(frontend): create InterceptorModal component and use that

--- apps/frontend/src/app/@modal/(.)register/page.tsx
+++ apps/frontend/src/app/@modal/(.)register/page.tsx
@@ -1,27 +1,7 @@
-"use client";
-
-import { Dialog } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
-
-// This duration is the same as the CSS transition duration
-// See https://github.com/radix-ui/themes/blob/main/packages/radix-ui-themes/src/components/dialog.css
-const ExitAnimationDuration = 150;
+import InterceptorModal from "../../../components/interceptor-modal";
 
 export default function Page() {
-  const router = useRouter();
-
   return (
-    <Dialog.Root
-      defaultOpen
-      onOpenChange={(open) => {
-        setTimeout(() => {
-          !open && router.back();
-        }, ExitAnimationDuration);
-      }}
-    >
-      <Dialog.Content>
-        <div>This is the Register Page in a Dialog</div>
-      </Dialog.Content>
-    </Dialog.Root>
+    <InterceptorModal title="Register">Register a new account</InterceptorModal>
   );
 }
---
+++ apps/frontend/src/components/interceptor-modal.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Dialog } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
+import type { PropsWithChildren } from "react";
+
+// This duration is the same as the CSS transition duration
+// See https://github.com/radix-ui/themes/blob/main/packages/radix-ui-themes/src/components/dialog.css
+const ExitAnimationDuration = 150;
+
+type Props = PropsWithChildren & {
+  title?: string;
+};
+export default function InterceptorModal({ children, title }: Props) {
+  const router = useRouter();
+
+  return (
+    <Dialog.Root
+      defaultOpen
+      onOpenChange={(open) => {
+        setTimeout(() => {
+          !open && router.back();
+        }, ExitAnimationDuration);
+      }}
+    >
+      <Dialog.Content>
+        {title ? <Dialog.Title>{title}</Dialog.Title> : null}
+        {children}
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+}
