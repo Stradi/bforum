851df97269df153665859872b2f72366ddbf171e
Batin Evirgen <batinevirgen@gmail.com>
2023-12-09 20:18:53 +0300

    fix(frontend): handle logout better

--- apps/frontend/src/app/(auth)/logout/page.tsx
+++ apps/frontend/src/app/(auth)/logout/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import useClient from "../../../hooks/use-client";
 import { logout } from "../../../lib/api/auth";
@@ -8,19 +7,18 @@ import { logout } from "../../../lib/api/auth";
 // Do we really need this page? I dunno, but it's fine, I guess.
 export default function Page() {
   const client = useClient();
-  const router = useRouter();
 
   useEffect(() => {
     if (!client) return;
 
     logout(client)
       .then(() => {
-        router.back();
+        window.location.href = "/";
       })
       .catch((err) => {
         throw err;
       });
-  }, [client, router]);
+  }, [client]);
 
   return null;
 }
