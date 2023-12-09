de36540065ceec0e6ad9ef6dd20e32ccd72160c3
Batin Evirgen <batinevirgen@gmail.com>
2023-12-09 23:04:42 +0300

    feat(frontend): add Header component for admin dashboard

---
+++ apps/frontend/src/app/admin/components/header.tsx
@@ -0,0 +1,33 @@
+import { Heading, Text } from "@radix-ui/themes";
+import type { ComponentPropsWithoutRef } from "react";
+import Container from "../../../components/container";
+import { cn } from "../../../utils/tw";
+
+type Props = ComponentPropsWithoutRef<"section"> & {
+  title: string;
+  description?: string;
+};
+export default function Header({
+  title,
+  description,
+  children,
+  className,
+  ...props
+}: Props) {
+  return (
+    <section
+      className={cn("border-b py-8 px-8 bg-neutral-100", className)}
+      {...props}
+    >
+      <Container className="px-4 flex justify-between items-center">
+        <div>
+          <Heading as="h1" mb="2" size="7">
+            {title}
+          </Heading>
+          {description ? <Text>{description}</Text> : null}
+        </div>
+        {children}
+      </Container>
+    </section>
+  );
+}
