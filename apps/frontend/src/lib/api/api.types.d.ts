ac822a3175c817bee0a7772d25e7f7876ab1921d
Batin Evirgen <batinevirgen@gmail.com>
2023-12-11 20:20:42 +0300

    feat(frontend): add API types

---
+++ apps/frontend/src/lib/api/api.types.d.ts
@@ -0,0 +1,78 @@
+type BaseApiObject = {
+  id: number;
+  created_at: number;
+  updated_at: number;
+};
+
+export type ApiAccount = BaseApiObject & {
+  username: string;
+  email: string;
+  display_name: string;
+  accountGroup: ApiAccountGroup[];
+};
+
+export type ApiGroup = BaseApiObject & {
+  name: string;
+  accountGroup: ApiAccountGroup[];
+  groupPermission: ApiGroupPermission[];
+};
+
+export type ApiAccountGroup = {
+  account_id: number;
+  group_id: number;
+  account: ApiAccount;
+  group: ApiGroup;
+};
+
+export type ApiPermission = BaseApiObject & {
+  name: string;
+  groupPermission: ApiGroupPermission[];
+};
+
+export type ApiGroupPermission = {
+  group_id: number;
+  permission_id: number;
+  group: ApiGroup;
+  permission: ApiPermission;
+};
+
+export type ApiNode = BaseApiObject & {
+  name: string;
+  slug: string;
+  description: string;
+
+  created_by: number;
+  creator: ApiAccount;
+
+  parent_id?: number;
+  parent?: ApiNode;
+
+  threads?: ApiThread[];
+};
+
+export type ApiThread = BaseApiObject & {
+  name: string;
+  slug: string;
+  replies: ApiReply[];
+
+  created_by: number;
+  creator: ApiAccount;
+
+  node_id: number;
+  node: ApiNode;
+};
+
+export type ApiReply = BaseApiObject & {
+  body: string;
+
+  thread_id: number;
+  thread: ApiThread;
+
+  reply_to_id?: number;
+  repliedTo?: ApiReply;
+
+  created_by: number;
+  creator: ApiAccount;
+
+  replies: ApiReply[];
+};
