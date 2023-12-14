2d4e64c933a055cf40f5c2df85a18f1ad0b4bd82
Batin Evirgen <batinevirgen@gmail.com>
2023-12-14 12:23:32 +0300

    chore(backend): forgot to add PermissionError

--- apps/backend/src/utils/errors.ts
+++ apps/backend/src/utils/errors.ts
@@ -73,3 +73,14 @@ export class ValidationError extends BaseError {
     });
   }
 }
+
+export class PermissionError extends BaseError {
+  constructor() {
+    super({
+      message: "You are not allowed to perform this action.",
+      code: "PERMISSION_ERROR",
+      statusCode: 403,
+      action: "Log in with an account that has the required permissions.",
+    });
+  }
+}
