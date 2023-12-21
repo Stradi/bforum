c6a3f64795d4126f6b40a4393660d21cb948d77f
Batin Evirgen <batinevirgen@gmail.com>
2023-12-21 17:13:08 +0300

    feat(frontend): display top level nodes in homepage

--- apps/frontend/src/app/(app)/page.tsx
+++ apps/frontend/src/app/(app)/page.tsx
@@ -1,3 +1,27 @@
-export default function Page() {
-  return <div>Welcome!</div>;
+import type { ApiNode } from "@lib/api/api.types";
+import createServerComponentClient from "@lib/api/client/create-server-component-client";
+import Container from "@components/container";
+import Node from "@components/node";
+
+export default async function Page() {
+  const client = await createServerComponentClient();
+  const nodes = await client.sendRequest<{
+    message: string;
+    payload: ApiNode[];
+  }>("/api/v1/nodes?with_children=1&with_thread_count=1");
+
+  if (!nodes.success) throw new Error(JSON.stringify(nodes.error));
+
+  const topLevelNodes = nodes.data.payload.filter((node) => !node.parent_id);
+
+  return (
+    <Container className="px-4">
+      <br />
+      <div className="border rounded-lg divide-y">
+        {topLevelNodes.map((node) => (
+          <Node key={node.id} node={node} />
+        ))}
+      </div>
+    </Container>
+  );
 }
