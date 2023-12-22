6050e277422e4031c4288b2c2f574cf79a469839
Batin Evirgen <batinevirgen@gmail.com>
2023-12-22 16:33:08 +0300

    style(frontend): use Header component in single node page and add New Thread button

--- apps/frontend/src/app/(app)/n/[slug]/page.tsx
+++ apps/frontend/src/app/(app)/n/[slug]/page.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Link from "next/link";
 import type { ApiNode } from "@lib/api/api.types";
 import createServerComponentClient from "@lib/api/client/create-server-component-client";
 import Node from "@components/node";
 import Container from "@components/container";
+import Header from "@components/header";
+import { Button } from "@components/ui/button";
 
 type Props = {
   params: {
@@ -19,22 +22,27 @@ export default async function Page({ params: { slug } }: Props) {
   if (!node.success) throw new Error(JSON.stringify(node.error));
 
   return (
-    <Container className="px-4">
+    <div>
+      <Header
+        description={node.data.payload.description}
+        title={node.data.payload.name}
+      >
+        <Button asChild>
+          <Link href={`/n/${node.data.payload.slug}/new-thread`}>
+            New Thread
+          </Link>
+        </Button>
+      </Header>
       <br />
-      <header>
-        <h1 className="text-xl">{node.data.payload.name}</h1>
-        <p className="text-sm text-neutral-500">
-          {node.data.payload.description}
-        </p>
-      </header>
-      <br />
-      {node.data.payload.children?.length ? (
-        <div className="divide-y rounded-lg border">
-          {node.data.payload.children.map((n) => (
-            <Node key={n.id} slug={n.slug} />
-          ))}
-        </div>
-      ) : null}
-    </Container>
+      <Container className="px-4">
+        {node.data.payload.children?.length ? (
+          <div className="divide-y rounded-lg border">
+            {node.data.payload.children.map((n) => (
+              <Node key={n.id} slug={n.slug} />
+            ))}
+          </div>
+        ) : null}
+      </Container>
+    </div>
   );
 }
