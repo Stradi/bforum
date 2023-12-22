b7a090dd0ef77b2af2bbe055fcd4248ec9ac38a5
Batin Evirgen <batinevirgen@gmail.com>
2023-12-22 13:58:02 +0300

    style(frontend): add Header to index page

--- apps/frontend/src/app/(app)/page.tsx
+++ apps/frontend/src/app/(app)/page.tsx
@@ -2,6 +2,7 @@ import type { ApiNode } from "@lib/api/api.types";
 import createServerComponentClient from "@lib/api/client/create-server-component-client";
 import Container from "@components/container";
 import Node from "@components/node";
+import Header from "@components/header";
 
 export default async function Page() {
   const client = await createServerComponentClient();
@@ -15,13 +16,19 @@ export default async function Page() {
   const topLevelNodes = nodes.data.payload.filter((node) => !node.parent_id);
 
   return (
-    <Container className="px-4">
+    <div>
+      <Header
+        description={`bForum currently has ${nodes.data.payload.length} nodes. You can browse, create threads, and reply to threads in any of them.`}
+        title="All Forums"
+      />
       <br />
-      <div className="divide-y rounded-lg border">
-        {topLevelNodes.map((node) => (
-          <Node key={node.id} node={node} />
-        ))}
-      </div>
-    </Container>
+      <Container className="px-4">
+        <div className="divide-y rounded-lg border">
+          {topLevelNodes.map((node) => (
+            <Node key={node.id} node={node} />
+          ))}
+        </div>
+      </Container>
+    </div>
   );
 }
